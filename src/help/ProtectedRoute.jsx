717716bc968022de853c0bc84f0b4a5475293f97
RicardoNardaoBelmonte <nardaoricardo@gmail.com>
2025-07-29 13:00:01 -0300

    Adding helper components that will be used  on other pages of the site

---
+++ src/help/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { userContext } from '../UserContext'
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({children}) => {
+
+    const {login} = React.useContext(userContext);
+
+  return login ? (children) : (<Navigate to='/login'/>);
+}
+
+export default ProtectedRoute
